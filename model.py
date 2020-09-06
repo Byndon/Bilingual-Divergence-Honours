c4b12aed68353cbcd1642a8cd0d6e74fee9766df
Callan Bindon <callan@BINDON>
2020-09-07 00:39:04 +0800

    updated model.py

--- model.py
+++ model.py
@@ -1,3 +1,14 @@
 #!/Documents/2020Honours/ABM/py3venv/bin
 import mesa
 import networkx
+# look at pyCX https://github.com/hsayama/PyCX
+# look at pyNSim https://umwrg.github.io/pynsim/architecture.html
+
+
+class Agent:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        pass
+
+
