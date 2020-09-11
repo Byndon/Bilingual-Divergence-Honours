fdf47d19e7ca947a6f57c7d6c7b451ef8a050511
Callan Bindon <callan@BINDON>
2020-09-11 22:45:14 +0800

    added the kml files for the polygons of languages in use, and the centroids for those languages

--- model.py
+++ model.py
@@ -1,14 +1,14 @@
 #!/Documents/2020Honours/ABM/py3venv/bin
 import mesa
+from mesa import Agent, Model
 import networkx
 # look at pyCX https://github.com/hsayama/PyCX
 # look at pyNSim https://umwrg.github.io/pynsim/architecture.html
 
 
-class Agent:
+class LanguageAgent(Agent):
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        super().__init__(x, y)
         pass
 
 
