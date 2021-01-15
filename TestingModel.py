ccc97c1114e8b1cc9a1774e8ab52b80c414e4a23
Callan Bindon <callan@BINDON>
2021-01-15 11:52:02 +0800

    Made code neater

--- TestingModel.py
+++ TestingModel.py
@@ -1,36 +1,38 @@
 # define language objects.
 # i don't currently care about the words and stuff
 from model import DivergenceModel
-from model import language
+from model import Language
 from model import Community
 import networkx as nx
 
-Language1 = language("Language1")
-Language2 = language("Language2")
-Language3 = language("Language3")
-Language4 = language("Language4")
-Language5 = language("Language5")
-Language6 = language("Language6")
+language1 = Language("Language1")
+language2 = Language("Language2")
+language3 = Language("Language3")
+language4 = Language("Language4")
+language5 = Language("Language5")
+language6 = Language("Language6")
 
 # make a list of the languages to give to the model.
-languageList = [Language1, Language2, Language3, Language4, Language5, Language6]
+languageList = [language1, language2, language3, language4, language5, language6]
 
 # input some meanings and forms into the languages
-meaning_I = ("I", [("A", 10, 0), ("Ay", 10, 0), ("Ei", 30, 0)])
-meaning_you = ("You", [("Yuh", 14, 0), ("Ye", 76, 0), ("thou", 32, 0)])
-meaning_we = ("We", [("Wey", 34, 0), ("Wi", 76, 0), ("Wir", 30, 0)])
 for eachlanguage in languageList:
-    eachlanguage.add_meaning(meaning_I[0], meaning_I[1])
-    eachlanguage.add_meaning(meaning_you[0], meaning_you[1])
-    eachlanguage.add_meaning(meaning_we[0], meaning_we[1])
+    eachlanguage.formMeaningDict = {}
+
+language1.add_meaning("Lizard", ([("wiri-wiri", 75, 0), ("mirdi", 10, 0)]))
+language2.add_meaning("Lizard", ([("wiri-wiri", 15, 0)]))
+language3.add_meaning("Lizard", ([("wiri-wiri", 50, 0), ("mirdi", 10, 0)]))
+language4.add_meaning("Lizard", ([("julirri", 80, 0), ("jindararda", 40, 0)]))
+language5.add_meaning("Lizard", ([("jindararda", 70, 0), ("wiri-wiri", 50, 0)]))
+language6.add_meaning("Lizard", ([("mirdi", 60, 0), ("jindararda", 60, 0)]))
 
 # define the communities which
-community1 = Community("Com1", Language1)
-community2 = Community("Com2", Language2)
-community3 = Community("Com3", Language3, 300)
-community4 = Community("Com4", Language4, 141)
-community5 = Community("Com5", Language5)
-community6 = Community("Com6", Language6)
+community1 = Community("Com1", language1, 10)
+community2 = Community("Com2", language2, 10)
+community3 = Community("Com3", language3, 10)
+community4 = Community("Com4", language4, 10)
+community5 = Community("Com5", language5, 10)
+community6 = Community("Com6", language6, 10)
 
 communityList = [community1, community2, community3, community4, community5, community6]
 # create network
@@ -42,7 +44,7 @@ socialNet.add_nodes_from(communityList)
 socialNet.add_weighted_edges_from([(community1, community6, 0.125), (community6, community5, 0.5), (community1, community5, 0.1), (community2, community5, 0.1), (community6, community4, 0.01), (community2, community4, 0.4)])
 
 
-# make the model. 
-testingmodel = DivergenceModel(languageList, communityList, socialNet)
+# make the model.
+testingModel = DivergenceModel(languageList, communityList, socialNet, 0.54, 1)
 # step the model once.
-testingmodel.step()
+testingModel.step()
